a8c37ea5a564c4ee555c239be1fea728760c8f6a
Karolina <karolinarogusz@onet.pl>
2020-08-11 00:09:24 +0200

    extract menu component

--- src/App.js
+++ src/App.js
@@ -3,35 +3,14 @@ import MainPage from "./components/main-page.jsx";
 import Series from "./components/series.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import {Spring} from "react-spring/renderprops";
-import styled from "styled-components";
 import Menu from "./components/menu.jsx";
 import Plot from "./components/plot.jsx";
 import Character from "./components/characters.jsx";
 import Facts from "./components/facts.jsx";
 import HomeIcon from "./components/home-icon.jsx";
+import MenuIcon from "./components/menu-icon.jsx";
 import seasonPlot from "./resources/seasons.js";
 import moviePlot from "./resources/movie.js";
-const arrow = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAmUlEQVRIie2UsQ3CMAAEnzRJwxigVAxDjdgpDTtkF6RkkxQZ4CiIEstyKLC/ghvgT37bL/0pBVADHXB0hJ+AgTdlBcAVmNgoIwCapZKYfAFwBsZEeL4AuAHzTvj3gg+VZHFYwltJvaRL1vETVMBd0tMRLkmVIzTEXtGK65JTIs8zjSS+jxZIfFMRiTxjF0l8cx1IGuBhE/wuL4q+v+UDi/jyAAAAAElFTkSuQmCC";
-
-const MenuButtonDiv = styled.div`
-position: absolute;
-top: 30px;
-left: 10px;
-transform: translateY(-50%);
-z-index: 9999;
-font: 0.75rem NewBaskervilleExpOdC; 
-& button{
-  border: none;
-  color: white;
-  background-color: transparent;
-  font: 1.5rem NewBaskervilleExpOdC;
-  letter-spacing: .2vw;
-  outline: none;
-  & img{
-    opacity: 0.3;
-  }
-}
-`;
 
 class App extends React.Component {
   constructor(props){
@@ -60,15 +39,7 @@ class App extends React.Component {
             {style => 
               <div >
                 <HomeIcon style = {style}/>
-                <MenuButtonDiv>
-                  <button 
-                    type = "button" 
-                    onClick = {this.toogleMenu}
-                    style = {style}
-                  >
-                    {this.state.menuDisabled ? "Menu" : <img src={arrow} alt = ""/>}
-                  </button>
-                </MenuButtonDiv >
+                <MenuIcon style = {style} toogleMenu= {this.toogleMenu} menuDisabled={this.state.menuDisabled} />
               </div>
             }
           </Spring>
---
+++ src/components/menu-icon.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const arrow = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAAAmUlEQVRIie2UsQ3CMAAEnzRJwxigVAxDjdgpDTtkF6RkkxQZ4CiIEstyKLC/ghvgT37bL/0pBVADHXB0hJ+AgTdlBcAVmNgoIwCapZKYfAFwBsZEeL4AuAHzTvj3gg+VZHFYwltJvaRL1vETVMBd0tMRLkmVIzTEXtGK65JTIs8zjSS+jxZIfFMRiTxjF0l8cx1IGuBhE/wuL4q+v+UDi/jyAAAAAElFTkSuQmCC";
+
+const MenuButtonDiv = styled.div`
+position: absolute;
+top: 30px;
+left: 10px;
+transform: translateY(-50%);
+z-index: 9999;
+font: 0.75rem NewBaskervilleExpOdC; 
+& button{
+  border: none;
+  color: white;
+  background-color: transparent;
+  font: 1.5rem NewBaskervilleExpOdC;
+  letter-spacing: .2vw;
+  outline: none;
+  & img{
+    opacity: 0.3;
+  }
+}
+`;
+
+const MenuIcon = (props) => {
+  return(
+    <MenuButtonDiv>
+      <button 
+        type = "button" 
+        onClick = {props.toogleMenu}
+        style = {props.style}
+      >
+        {props.menuDisabled ? "Menu" : <img src={arrow} alt = ""/>}
+      </button>
+    </MenuButtonDiv >
+  );
+};
+
+MenuIcon.propTypes = {
+  toogleMenu: PropTypes.func,
+  style: PropTypes.string,
+  menuDisabled: PropTypes.bool
+}; 
+
+export default MenuIcon;
