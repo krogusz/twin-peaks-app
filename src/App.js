4066f9e00d46501316b90dbdd43eb83621250995
Karolina <karolinarogusz@onet.pl>
2020-10-10 22:12:52 +0200

    add hook in App component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import MainPage from "./components/main-page.jsx";
 import Series from "./components/series.jsx";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -12,51 +12,41 @@ import MenuIcon from "./components/menu-icon.jsx";
 import seasonPlot from "./resources/seasons.json";
 import moviePlot from "./resources/movie.json";
 
-class App extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      menuDisabled: true
-    };
-    this.toogleMenu = this.toogleMenu.bind(this);
+const App = () => {
+  const [menuDisabled, setMenuDisabled] = useState(true);
+
+  const toogleMenu = () => {
+    setMenuDisabled(!menuDisabled)
   }
 
-  toogleMenu(){
-    this.setState({
-      menuDisabled: ! this.state.menuDisabled
-    });
-  }
-
-  render(){
-    return(
-      <div style = {{position: "relative"}}>
-        <Router>
-          <Spring 
-            config = {{tension:120, friction: 120, delay: 1300}}
-            from={{opacity: 0}}
-            to={{opacity: 1}}
-          >
-            {style => 
-              <div >
-                <HomeIcon style = {style}/>
-                <MenuIcon style = {style} toogleMenu= {this.toogleMenu} menuDisabled={this.state.menuDisabled} />
-              </div>
-            }
-          </Spring>
-          <Route exact path = "/" > <MainPage /> </Route>
-          <Route exact path = "/series"> <Series /> </Route>
-          <Route path = "/series/:id" render={(props) => <Plot {...props} plot = {seasonPlot} />}/> 
-          <Route path = "/movie" render={(props) => <Plot {...props} plot = {moviePlot} />}/>
-          <Route path = "/characters"> <Character /> </Route>
-          <Route path = "/interesting facts"> <Facts /> </Route>
-          <Menu 
-            disabled = {this.state.menuDisabled}
-            onClick = {this.toogleMenu}
-          /> 
-        </Router>
-      </div>
-    );
-  }
+  return(
+    <div style = {{position: "relative"}}>
+      <Router>
+        <Spring 
+          config = {{tension:120, friction: 120, delay: 1300}}
+          from={{opacity: 0}}
+          to={{opacity: 1}}
+        >
+          {style => 
+            <div >
+              <HomeIcon style = {style}/>
+              <MenuIcon style = {style} toogleMenu= {toogleMenu} menuDisabled={menuDisabled} />
+            </div>
+          }
+        </Spring>
+        <Route exact path = "/" > <MainPage /> </Route>
+        <Route exact path = "/series"> <Series /> </Route>
+        <Route path = "/series/:id" render={(props) => <Plot {...props} plot = {seasonPlot} />}/> 
+        <Route path = "/movie" render={(props) => <Plot {...props} plot = {moviePlot} />}/>
+        <Route path = "/characters"> <Character /> </Route>
+        <Route path = "/interesting facts"> <Facts /> </Route>
+        <Menu 
+          disabled = {menuDisabled}
+          onClick = {toogleMenu}
+        /> 
+      </Router>
+    </div>
+  );
 }
 
 export default App;
